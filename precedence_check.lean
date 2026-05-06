2d636f5f1abc086a850203ed006b2c0c1e2799b9
khanh <khanh.nguyen.contact@gmail.com>
2026-05-06 16:52:56 +0800

    auto_push

--- precedence_check.lean
+++
@@ -1,9 +0,0 @@
-import EL2.ParserCombinator
-
-open EL2.ParserCombinator
-
-def checkPrecedence (p1 p2 p3 : ParseFunc (List Char) String) :=
-  p1 ++ p2 || p3
-
-#check @OrElse.orElse
-#check (· <|> ·)
